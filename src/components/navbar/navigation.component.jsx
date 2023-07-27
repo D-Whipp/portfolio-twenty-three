ebc8c9c25553afbd602e29cf6d8612dd5e7cb846
David Whipple <dwhipp88@gmail.com>
2023-07-27 14:45:10 -0600

    initial commit

---
+++ src/components/navbar/navigation.component.jsx
@@ -0,0 +1,13 @@
+import './navigation.styles.css';
+
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className="navigation-container">
+            <p>I'm listening...</p>
+        </div>
+    );
+};
+
+export default Navigation;
