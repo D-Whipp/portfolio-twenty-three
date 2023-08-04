b5b027d001c1e4d5113473965a7633990bd5473e
David Whipple <dwhipp88@gmail.com>
2023-08-04 17:34:43 -0600

    setup projects page using map() iteration

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -1,59 +1,36 @@
 import './project-card.styles.css';
 
-import data from '../../data.json';
-
 import { Link } from 'react-router-dom';
 
 const ProjectCard = (props) => {
-    // console.log('props: ', props.data[0].name);
-    const projectInfo = props.data;
-    // const {name} = props.data;
-    // console.log('name: ', name)
-    // const {name} = projectInfo;
-    console.log('projectInfo: ', projectInfo);
-    // console.log(Object.keys(projectInfo))
-
 
     return (
         <div className="info-card-container">
             <div id="image-container">
-                <img src={data[0].imageUrl} alt={data[0].name} />
+                <img src={props.imgUrl} alt={props.name} />
             </div>
 
             <div id="info-text">
-                <h2>{data[0].name}</h2>
-                <h4>{data[0].technologyUsed}</h4>
+                <h2>{props.name}</h2>
+                <h4>{props.tech}</h4>
                 <div className="links-container">
                     <Link
                         className="link-item"
-                        to={`${data[0].deployedSite}`}
+                        to={`${props.projectSite}`}
                     >
                         Website
                     </Link>
                     <Link
                         className="link-item"
-                        to={`${data[0].githubRepository}`}
+                        to={`${props.projectRepo}`}
                     >
                         Repository
                     </Link>
                 </div>
-                <p>{data[0].briefExplanation}</p>
+                <p>{props.explanation}</p>
             </div>
         </div>
     );
 };
 
-// <div
-//     style={{ backgroundImage: `url(${data[0].imageUrl})` }}
-//     className="project-card-container"
-// >
-//     <div className="card-data">
-//         <h2>{data[0].name}</h2>
-//         <p>{data[0].githubRepository}</p>
-//         <p>{data[0].deployedSite}</p>
-//         <p>{data[0].technologyUsed}</p>
-//         <p>{data[0].briefExplanation}</p>
-//     </div>
-// </div>
-// <img src={data[0].imageUrl} alt={data[0].name} />
 export default ProjectCard;
--- src/routes/projects/projects.component.jsx
+++ src/routes/projects/projects.component.jsx
@@ -4,36 +4,22 @@ import Navigation from '../navbar/navigation.component';
 import ProjectCard from '../../components/project-card/project-card.component';
 
 import data from '../../data.json';
-// <h3 key={project.id}>{project.name}</h3>
-// const [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r] = data;
-// console.log('a:  ', a);
-// console.log('b:  ', b);
-// console.log('c:  ', c);
-// console.log('d:  ', d);
-// console.log('e:  ', e);
-// console.log('f:  ', f);
-// console.log('g:  ', g);
-// console.log('h:  ', h);
-// console.log('i:  ', i);
-// console.log('j:  ', j);
-// console.log('k:  ', k);
-// console.log('l:  ', l);
-// console.log('m:  ', m);
-// console.log('n:  ', n);
-// console.log('o:  ', o);
-// console.log('p:  ', p);
-// console.log('q:  ', q);
-// console.log('r:  ', r);
 
 const Projects = () => {
-    // console.log('data: ', data);
-
     return (
         <>
             <Navigation />
             <div className="projects-container">
                 {data.map((project) => (
-                    <ProjectCard key={project.id} data={data} />
+                    <ProjectCard
+                        key={project.id}
+                        name={project.name}
+                        imgUrl={project.imageUrl}
+                        projectRepo={project.githubRepository}
+                        projectSite={project.deployedSite}
+                        tech={project.technologyUsed}
+                        explanation={project.briefExplanation}
+                    />
                 ))}
 
                 <ProjectCard />
