653b737ff3c950b84a366fedaad42026e9328990
David Whipple <dwhipp88@gmail.com>
2023-08-07 13:59:00 -0600

    BUG FIX: fixed misbehavior when displaying projects

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -4,33 +4,39 @@ import { Link } from 'react-router-dom';
 
 const ProjectCard = (props) => {
     return (
-        <div className="info-card-container">
-            <div id="image-container">
-                <img src={props.imgUrl} alt={props.name} />
-            </div>
+        <>
+            {props.name ? (
+                <div className="info-card-container">
+                    <div id="image-container">
+                        <img src={props.imgUrl} alt={props.name} />
+                    </div>
 
-            <div id="info-text">
-                <h2>{props.name}</h2>
-                <h4>{props.tech}</h4>
-                <div className="links-container">
-                    <Link
-                        className="link-item"
-                        to={`${props.projectSite}`}
-                        target="_blank"
-                    >
-                        Website
-                    </Link>
-                    <Link
-                        className="link-item"
-                        to={`${props.projectRepo}`}
-                        target="_blank"
-                    >
-                        Repository
-                    </Link>
+                    <div id="info-text">
+                        <h2>{props.name}</h2>
+                        <h4>{props.tech}</h4>
+                        <div className="links-container">
+                            <Link
+                                className="link-item"
+                                to={`${props.projectSite}`}
+                                target="_blank"
+                            >
+                                Website
+                            </Link>
+                            <Link
+                                className="link-item"
+                                to={`${props.projectRepo}`}
+                                target="_blank"
+                            >
+                                Repository
+                            </Link>
+                        </div>
+                        <p>{props.explanation}</p>
+                    </div>
                 </div>
-                <p>{props.explanation}</p>
-            </div>
-        </div>
+            ) : (
+                <p></p>
+            )}
+        </>
     );
 };
 
